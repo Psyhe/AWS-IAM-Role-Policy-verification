66c894103de32d3c24ec43f921274da806146141
Psyhe <evarosa@interia.pl>
2024-03-31 18:36:40 +0200

    tests added, code changed accordingly

--- aws_json.py
+++ aws_json.py
@@ -1,29 +1,11 @@
 import json
+import re
 
 # I understand that in all cases, except of valid AWS::IAM::Role Policy data with asterisk (*) in 'Resource' field, the program will return True,
 # including cases with wrong data format.
 
-def additional_keys_exist(input_map, listed_keys):
-    return any(key not in listed_keys for key in input_map.keys())
-
-def verify_aws_json_format(json_data):
-
-    if "PolicyName" not in json_data:
-        print("Invalid AWS::IAM::Role Policy: lack of PolicyName")
-        return True
-    
-    if "PolicyDocument" not in json_data:
-        print("Invalid AWS::IAM::Role Policy: lack of PolicyDocument")
-        return True
-    
-    policy_document = json_data["PolicyDocument"]
-
-    if "Statement" not in policy_document.keys():
-        print("Invalid AWS::IAM::Role Policy: lack of Statement")
-        return True
-    
-    list_of_statements = policy_document["Statement"]
-    map_of_statements = list_of_statements[0]
+def check_if_asterisk_in_resource(data):
+    map_of_statements = data
 
     obligatory_keys = ["Effect", "Action"]
 
@@ -44,6 +26,74 @@ def verify_aws_json_format(json_data):
     else:
         print("Lack of asterisk (*) in 'Resource' field.")
         return True
+
+def additional_keys_exist(input_map, listed_keys):
+    return any(key not in listed_keys for key in input_map.keys())
+
+def check_pattern(pattern, s):
+    if re.fullmatch(pattern, s):
+        return True
+    else:
+        return False
+    
+def verify_policy_name_properties(policy_name):
+    if not (check_pattern(r"[\w+=,.@-]+", policy_name)):
+        print("Invalid AWS::IAM::Role Policy: wrong PolicyName format")
+        return True
+    
+    if len(policy_name) > 128:
+        print("Invalid AWS::IAM::Role Policy: PolicyName is too long")
+        return True
+    
+    return False
+
+def is_dict(s):
+    if type(s) is dict:
+        return True
+    else:
+        return False
+
+def verify_aws_json_format(json_data):
+
+    if "PolicyName" not in json_data:
+        print("Invalid AWS::IAM::Role Policy: lack of PolicyName")
+        return True
+    
+    if "PolicyDocument" not in json_data:
+        print("Invalid AWS::IAM::Role Policy: lack of PolicyDocument")
+        return True
+    
+    if len(json_data.keys()) > 2:
+        print("Invalid AWS::IAM::Role Policy: to many properties in JSON file")
+        return True
+    
+    if verify_policy_name_properties(json_data["PolicyName"]):
+        return True
+    
+    policy_document = json_data["PolicyDocument"]
+
+    print(policy_document)
+    print("policy document")
+
+    if is_dict(policy_document) == False:
+        print("Invalid AWS::IAM::Role Policy: wrong PolicyDocument format")
+        return True
+    
+    if additional_keys_exist(policy_document, ["Version", "Statement"]):
+        print("Invalid AWS::IAM::Role Policy: wrong PolicyDocument format - additional unrequired information")
+        return True
+
+    if "Statement" not in policy_document.keys():
+        print("Invalid AWS::IAM::Role Policy: lack of Statement")
+        return True
+    
+    list_of_statements = policy_document["Statement"]
+
+    for statement in list_of_statements:
+        if check_if_asterisk_in_resource(statement) == False:
+            return False
+    
+    return True
 
 def operations_on_json(json_data):
     json_data = json.loads(json_data.read())
--- tests.py
+++ tests.py
@@ -1,14 +1,156 @@
 import unittest
+import json
 from aws_json import verify_json_input
+from aws_json import verify_aws_json_format
 
-class TestStringMethods(unittest.TestCase):
-    def test_resource_with_asterisk(self):
-        self.assertEqual(verify_json_input("tests/resource_with_asterisk/json_example.json"), False)
+import aws_json
 
+class TestInvalidInput(unittest.TestCase):
     def test_invalid_json_format(self):
         self.assertEqual(verify_json_input("tests/invalid_json_format/invalid1.json"), True)
-
     
+    def test_invalid_input_fromat(self):
+        self.assertEqual(verify_json_input("tests/invalid_json_format/something.txt"), True)
+
+class TestCorrectInput(unittest.TestCase):
+    def test_resource_with_asterisk(self):
+        self.assertEqual(verify_json_input("tests/valid_json/json_example.json"), False)
+
+    def test_resource_without_asterisk_many_resources(self):
+        self.assertEqual(verify_json_input("tests/valid_json/example_without_asterisk.json"), True)
+
+    # def test_resource_with_asterisk_many_resources(self):
+    #     self.assertEqual(verify_json_input("tests/valid_json/example_with_asterisk_certain_resource.json"), False)
+
+class TestJsonString(unittest.TestCase):
+    def test_wrong_policy_definition(self):
+        json_string = """
+        {
+            "PolicyName": "root",
+            "PolicyDocument": {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Sid": "IamListAccess",
+                        "Effect": "Allow",
+                        "Action": [
+                            "iam:ListRoles",
+                            "iam:ListUsers"
+                        ],
+                        "Resource": "*"
+                    }
+                ]
+            },
+            "PolicyAdditional": {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Sid": "IamListAccess",
+                        "Effect": "Allow",
+                        "Action": [
+                            "iam:ListRoles",
+                            "iam:ListUsers"
+                        ],
+                        "Resource": "*"
+                    }
+                ]
+            }
+        }
+        """
+        json_data = json.loads(json_string)
+        self.assertEqual(verify_aws_json_format(json_data), True)
+
+    def test_wrong_policy_name(self):
+        json_string = """
+        {
+            "PolicyName": "root!",
+            "PolicyDocument": {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Sid": "IamListAccess",
+                        "Effect": "Allow",
+                        "Action": [
+                            "iam:ListRoles",
+                            "iam:ListUsers"
+                        ],
+                        "Resource": "*"
+                    }
+                ]
+            }
+        }
+        """
+        json_data = json.loads(json_string)
+        self.assertEqual(verify_aws_json_format(json_data), True)
+
+    def test_wrong_policy_document_format(self):
+        json_string = """
+        {
+            "PolicyName": "root",
+            "PolicyDocument": "document"
+        }
+        """
+        json_data = json.loads(json_string)
+        self.assertEqual(verify_aws_json_format(json_data), True)
+
+    def test_lack_of_statement(self):
+        json_string = """
+        {
+            "PolicyName": "root",
+            "PolicyDocument": {
+                "Version": "2012-10-17"
+            }
+        }
+        """
+        json_data = json.loads(json_string)
+        self.assertEqual(verify_aws_json_format(json_data), True)
+
+    def test_additional_info_in_policy_document(self):
+        json_string = """
+        {
+            "PolicyName": "root",
+            "PolicyDocument": {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Sid": "IamListAccess",
+                        "Effect": "Allow",
+                        "Action": [
+                            "iam:ListRoles",
+                            "iam:ListUsers"
+                        ],
+                        "Resource": "*"
+                    }
+                ],
+                "Additional": "info"
+            }
+        }
+        """
+        json_data = json.loads(json_string)
+        self.assertEqual(verify_aws_json_format(json_data), True)
+
+class TestPolicyName(unittest.TestCase):
+    def test_too_long_policy_name(self):
+        self.assertEqual(aws_json.verify_policy_name_properties("a" * 129), True)
+
+    def test_wrong_policy_name(self):
+        self.assertEqual(aws_json.verify_policy_name_properties("root!"), True)
+
+    def test_wrong_policy_name2(self):
+        self.assertEqual(aws_json.verify_policy_name_properties("root*"), True)
+
+    def test_empty_policy_name(self):
+        self.assertEqual(aws_json.verify_policy_name_properties(""), True)
+
+    def test_json_name_format(self):
+        self.assertEqual(aws_json.verify_policy_name_properties("{ Resource: '*',}"), True)
+
+    def test_correct_policy_name(self):
+        self.assertEqual(aws_json.verify_policy_name_properties("root"), False)
+
+    def test_correct_policy_name2(self):
+        self.assertEqual(aws_json.verify_policy_name_properties("ROOT123@"), False)
+ 
 
 def run_tests():
     unittest.main()
